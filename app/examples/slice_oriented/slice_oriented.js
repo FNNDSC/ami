1c6cd804eb483f9ddda26b1add7155eec5968524
nicolasrannou <npm.rannou@gmail.com>
2015-03-04 15:14:10 +0100

    pushing new demos, refactoring in progress....

---
+++ app/examples/slice_oriented/slice_oriented.js
@@ -0,0 +1,174 @@
+'use strict';
+
+var VJS = VJS || {};
+
+var Stats = Stats || {};
+// standard global variables
+var controls, renderer, stats, volume, mouse, raycaster, plane, threeD, probe, rasijk, ijkdimensions;
+
+// FUNCTIONS
+function init(slice) {
+    function onDocumentMouseMove(event) {
+        // calculate mouse position in normalized device coordinates
+        // (-1 to +1) for both components
+        mouse.x = (event.clientX / threeD.offsetWidth) * 2 - 1;
+        mouse.y = -(event.clientY / threeD.offsetHeight) * 2 + 1;
+    }
+
+    // this function is executed on each animation frame
+    function animate() {
+
+        raycaster.setFromCamera(mouse, camera);
+
+        // calculate objects intersecting the picking ray
+        var intersects = raycaster.intersectObjects(scene.children);
+
+        //window.console.log(intersects);
+
+        for (var intersect in intersects) {
+            var ras = new THREE.Vector3().copy(intersects[intersect].point);
+            if (plane.uuid === intersects[intersect].object.uuid) {
+                // convert point to IJK
+                var ijk = intersects[intersect].point.applyMatrix4(rasijk);
+                ijk.x += 0.5;
+                ijk.y += 0.5;
+                ijk.z += 0.5;
+                // get value!
+                if (ijk.x >= 0 && ijk.y >= 0 && ijk.z >= 0 &&
+                    ijk.x <= ijkdimensions.x &&
+                    ijk.y <= ijkdimensions.y &&
+                    ijk.z <= ijkdimensions.z) {
+
+                    var value = vjsVolumeCore.getValue(Math.floor(ijk.x), Math.floor(ijk.y), Math.floor(ijk.z), 0, false);
+                    probe.update(ras, ijk, value);
+                }
+
+                break;
+            }
+        }
+
+        // render
+        controls.update();
+        renderer.render(scene, camera);
+        stats.update();
+
+        // request new frame
+        requestAnimationFrame(function() {
+            animate();
+        });
+    }
+
+    // renderer
+    threeD = document.getElementById('r3d');
+    var renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
+    renderer.setSize(threeD.offsetWidth, threeD.offsetHeight);
+    renderer.setClearColor(0xB0BEC5, 1);
+    threeD.appendChild(renderer.domElement);
+
+    // stats
+    stats = new Stats();
+    threeD.appendChild(stats.domElement);
+
+    // probe
+    probe = new VJS.Widgets.Probe();
+    threeD.appendChild(probe.domElement);
+
+    // scene
+    var scene = new THREE.Scene();
+    // camera
+    var camera = new THREE.OrthographicCamera(threeD.offsetWidth / -2, threeD.offsetWidth / 2, threeD.offsetHeight / 2, threeD.offsetHeight / -2, 1, 10000000);
+    camera.position.x = 400;
+    // controls
+    controls = new THREE.OrbitControls2D(camera, renderer.domElement);
+    controls.noRotate = true;
+
+
+    // create volume object
+
+    rasijk = VJS.Adaptor.Xtk2ThreejsMat4(volume.Qh);
+    var ijkras = new THREE.Matrix4().getInverse(rasijk);
+    ijkdimensions = VJS.Adaptor.Xtk2ThreejsVec3(volume.ca);
+    var rasdimensions = VJS.Adaptor.Xtk2ThreejsVec3(volume.gb);
+    var rascenter = VJS.Adaptor.Xtk2ThreejsVec3(volume.s);
+    var rasorigin = VJS.Adaptor.Xtk2ThreejsVec3(volume.Ea);
+
+    // Create RAS object
+    var ras = {
+        'origin': rasorigin,
+        'center': rascenter,
+        'dimensions': rasdimensions,
+        'spacing': null,
+        'boundingbox': [
+            new THREE.Vector3(rascenter.x - rasdimensions.x / 2, rascenter.y - rasdimensions.y / 2, rascenter.z - rasdimensions.z / 2),
+            new THREE.Vector3(rascenter.x + rasdimensions.x / 2, rascenter.y + rasdimensions.y / 2, rascenter.z + rasdimensions.z / 2)
+        ]
+    };
+
+    // need ijk object as well
+    var ijk = {
+        'origin': null,
+        'center': null,
+        'dimensions': ijkdimensions,
+        'spacing': null
+    };
+
+    var transforms = {
+        'ijk2ras': ijkras,
+        'ras2ijk': rasijk
+    };
+
+    // Create VJS Volume Core and View
+    var vjsVolumeCore = new VJS.Volume.Core(volume.J, volume.max, volume.min, transforms, ijk, ras);
+
+    // Create Slice
+
+    // get texture from object
+    var tSize = 4096.0;
+    var tNumber = 4;
+    vjsVolumeCore.createTexture(tNumber, tSize);
+
+    var normalorigin = VJS.Adaptor.Xtk2ThreejsVec3(slice.z);
+    var normaldirection = VJS.Adaptor.Xtk2ThreejsVec3(slice.ec);
+
+    // Create VJS Slice Core and View
+    var vjsSliceCore = new VJS.Slice.Core(normalorigin, normaldirection, vjsVolumeCore);
+    vjsSliceCore.Slice();
+
+    // Get View from slice!
+
+    // Plane filled with volume's texture
+    var vjsSliceView = new VJS.Slice.View(vjsSliceCore);
+    vjsSliceView._Convention = 'RADIOLOGY';
+    vjsSliceView._Orientation = 'SAGITTAL';
+    plane = vjsSliceView.RASSlice(tSize, tNumber);
+    scene.add(plane);
+
+    // hook up mouse events
+    raycaster = new THREE.Raycaster();
+    mouse = new THREE.Vector2();
+    renderer.domElement.addEventListener('mousemove', onDocumentMouseMove, false);
+
+    // start animation
+    animate();
+}
+
+window.onload = function() {
+    // create the 2D renderers (just load tand parse the file...)
+    var sliceX = new X.renderer2D();
+    sliceX.container = 'sliceX';
+    sliceX.orientation = 'X';
+    sliceX.init();
+
+    volume = new X.volume();
+    volume.file = '../../data/lesson17_cropped.nii.gz';
+    // volume.file = 'data/CT.nii.gz';
+    volume.reslicing = true;
+    sliceX.add(volume);
+
+    sliceX.render();
+    sliceX.onShowtime = function() {
+        init(volume.children[0].c[volume.indexX]);
+    };
+};
--- app/modules/widgets/boxProbe/ProbeROI.js
+++ app/modules/widgets/boxProbe/ProbeROI.js
@@ -12,7 +12,79 @@ VJS.Widgets.ProbeROI = function() {
     this.varianceContainer = null;
     this.chartContainer = null;
 
+    // active element
+    this.handles = [];
+    this.selected = null;
+    this.roi = null;
+
+    this.volumeCore = null;
+
     this.createDomElement();
+};
+
+
+VJS.Widgets.ProbeROI.prototype.updateROI = function() {
+
+    // need to compute it properly
+    var geometry = new THREE.Geometry();
+    geometry.vertices.push(new THREE.Vector3(this.handles[0].position.x, this.handles[0].position.y, this.handles[0].position.z));
+    geometry.vertices.push(new THREE.Vector3(this.handles[0].position.x, this.handles[1].position.y, this.handles[0].position.z));
+    geometry.vertices.push(new THREE.Vector3(this.handles[0].position.x, this.handles[1].position.y, this.handles[1].position.z));
+    geometry.vertices.push(new THREE.Vector3(this.handles[0].position.x, this.handles[0].position.y, this.handles[1].position.z));
+    geometry.vertices.push(new THREE.Vector3(this.handles[0].position.x, this.handles[0].position.y, this.handles[0].position.z));
+    geometry.verticesNeedUpdate = true;
+
+    if (!this.roi) {
+        var material = new THREE.LineBasicMaterial({
+            color: 0xff00f0,
+            linewidth: 2
+        });
+        material.polygonOffset = true;
+        material.polygonOffsetFactor = 1;
+        material.polygonOffsetUnits = 1;
+        this.roi = new THREE.Line(geometry, material);
+    } else {
+        this.roi.geometry = geometry;
+    }
+    return this.roi;
+};
+
+
+VJS.Widgets.ProbeROI.prototype.setVolumeCore = function(object) {
+    this.volumeCore = object;
+};
+
+VJS.Widgets.ProbeROI.prototype.setSelected = function(object) {
+    this.selected = object;
+};
+
+VJS.Widgets.ProbeROI.prototype.getSelected = function() {
+    return this.selected;
+};
+
+VJS.Widgets.ProbeROI.prototype.updateSelected = function(ras) {
+
+    // change selected object position..!
+    this.selected.position.x = ras.x;
+    this.selected.position.y = ras.y;
+    this.selected.position.z = ras.z;
+};
+
+VJS.Widgets.ProbeROI.prototype.getHandles = function() {
+    return this.handles;
+};
+
+VJS.Widgets.ProbeROI.prototype.addHandle = function(ras) {
+    var sphereGeometry = new THREE.SphereGeometry(1);
+    var material = new THREE.MeshBasicMaterial({
+        color: 0xff00f0
+    });
+    var sphere = new THREE.Mesh(sphereGeometry, material);
+    sphere.applyMatrix(new THREE.Matrix4().makeTranslation(ras.x, ras.y, ras.z));
+    sphere.name = 'probeROI';
+    this.handles.push(sphere);
+
+    return sphere;
 };
 
 VJS.Widgets.ProbeROI.prototype.createDomElement = function() {
@@ -38,7 +110,7 @@ VJS.Widgets.ProbeROI.prototype.createDomElement = function() {
     this.varianceContainer.setAttribute('id', 'VJSProbeROIVariance');
 
     // small trick so chart not drawn on top of it.
-    this.update({});
+    this.updateCore({});
 
     // Chart elements to display all info at once
     this.chartContainer = document.createElement('div');
@@ -61,9 +133,166 @@ VJS.Widgets.ProbeROI.prototype.createDomElement = function() {
     this.domElement.appendChild(this.chartContainer);
 };
 
+VJS.Widgets.ProbeROI.prototype.update = function() {
+
+    // need to compute 4 points of the box from 2 points in diagonal!
+    var h1 = this.handles[0].position;
+    var h2 = this.handles[1].position;
+
+    // computer center (might be used to drag it later)
+    var center = new THREE.Vector3(h1.x + (h2.x - h1.x) / 2,
+        h1.y + (h2.y - h1.y) / 2,
+        h1.z + (h2.z - h1.z) / 2);
+
+    // box's sides length
+    var dx = Math.sqrt((h2.x - h1.x) * (h2.x - h1.x)) / 2;
+    var dy = Math.sqrt((h2.y - h1.y) * (h2.y - h1.y)) / 2;
+    var dz = Math.sqrt((h2.z - h1.z) * (h2.z - h1.z)) / 2;
+
+    // 8 RAS corners
+    var c0 = new THREE.Vector3(center.x - dx, center.y - dy, center.z - dz);
+    var c1 = new THREE.Vector3(center.x - dx, center.y - dy, center.z + dz);
+    var c2 = new THREE.Vector3(center.x - dx, center.y + dy, center.z - dz);
+    var c3 = new THREE.Vector3(center.x - dx, center.y + dy, center.z + dz);
+    var c4 = new THREE.Vector3(center.x + dx, center.y - dy, center.z - dz);
+    var c5 = new THREE.Vector3(center.x + dx, center.y - dy, center.z + dz);
+    var c6 = new THREE.Vector3(center.x + dx, center.y + dy, center.z - dz);
+    var c7 = new THREE.Vector3(center.x + dx, center.y + dy, center.z + dz);
+
+    // 8 IJK corners
+    var ijk0 = new THREE.Vector3(c0.x, c0.y, c0.z).applyMatrix4(this.volumeCore._Transforms.ras2ijk);
+    ijk0.x = Math.floor(ijk0.x + 0.5);
+    ijk0.y = Math.floor(ijk0.y + 0.5);
+    ijk0.z = Math.floor(ijk0.z + 0.5);
+
+    var ijk1 = new THREE.Vector3(c1.x, c1.y, c1.z).applyMatrix4(this.volumeCore._Transforms.ras2ijk);
+    ijk1.x = Math.floor(ijk1.x + 0.5);
+    ijk1.y = Math.floor(ijk1.y + 0.5);
+    ijk1.z = Math.floor(ijk1.z + 0.5);
+
+    var ijk2 = new THREE.Vector3(c2.x, c2.y, c2.z).applyMatrix4(this.volumeCore._Transforms.ras2ijk);
+    ijk2.x = Math.floor(ijk2.x + 0.5);
+    ijk2.y = Math.floor(ijk2.y + 0.5);
+    ijk2.z = Math.floor(ijk2.z + 0.5);
+
+    var ijk3 = new THREE.Vector3(c3.x, c3.y, c3.z).applyMatrix4(this.volumeCore._Transforms.ras2ijk);
+    ijk3.x = Math.floor(ijk3.x + 0.5);
+    ijk3.y = Math.floor(ijk3.y + 0.5);
+    ijk3.z = Math.floor(ijk3.z + 0.5);
+
+    var ijk4 = new THREE.Vector3(c4.x, c4.y, c4.z).applyMatrix4(this.volumeCore._Transforms.ras2ijk);
+    ijk4.x = Math.floor(ijk4.x + 0.5);
+    ijk4.y = Math.floor(ijk4.y + 0.5);
+    ijk4.z = Math.floor(ijk4.z + 0.5);
+
+    var ijk5 = new THREE.Vector3(c5.x, c5.y, c5.z).applyMatrix4(this.volumeCore._Transforms.ras2ijk);
+    ijk5.x = Math.floor(ijk5.x + 0.5);
+    ijk5.y = Math.floor(ijk5.y + 0.5);
+    ijk5.z = Math.floor(ijk5.z + 0.5);
+
+    var ijk6 = new THREE.Vector3(c6.x, c6.y, c6.z).applyMatrix4(this.volumeCore._Transforms.ras2ijk);
+    ijk6.x = Math.floor(ijk6.x + 0.5);
+    ijk6.y = Math.floor(ijk6.y + 0.5);
+    ijk6.z = Math.floor(ijk6.z + 0.5);
+
+    var ijk7 = new THREE.Vector3(c7.x, c7.y, c7.z).applyMatrix4(this.volumeCore._Transforms.ras2ijk);
+    ijk7.x = Math.floor(ijk7.x + 0.5);
+    ijk7.y = Math.floor(ijk7.y + 0.5);
+    ijk7.z = Math.floor(ijk7.z + 0.5);
+
+    // get IJK BBox and look + test each
+    var ijkMin = new THREE.Vector3(Math.min(ijk0.x, ijk1.x, ijk2.x, ijk3.x, ijk4.x, ijk5.x, ijk5.x, ijk7.x),
+        Math.min(ijk0.y, ijk1.y, ijk2.y, ijk3.y, ijk4.y, ijk5.y, ijk5.y, ijk7.y),
+        Math.min(ijk0.z, ijk1.z, ijk2.z, ijk3.z, ijk4.z, ijk5.z, ijk5.z, ijk7.z));
+
+    var ijkMax = new THREE.Vector3(Math.max(ijk0.x, ijk1.x, ijk2.x, ijk3.x, ijk4.x, ijk5.x, ijk5.x, ijk7.x),
+        Math.max(ijk0.y, ijk1.y, ijk2.y, ijk3.y, ijk4.y, ijk5.y, ijk5.y, ijk7.y),
+        Math.max(ijk0.z, ijk1.z, ijk2.z, ijk3.z, ijk4.z, ijk5.z, ijk5.z, ijk7.z));
+
+    // fix ijk out of range
+    if (ijkMin.x < 0) {
+        ijkMin.x = 0;
+    }
+    if (ijkMin.x >= this.volumeCore._IJK.dimensions.x) {
+        ijkMin.x = this.volumeCore._IJK.dimensions.x - 1;
+    }
+    if (ijkMin.y < 0) {
+        ijkMin.y = 0;
+    }
+    if (ijkMin.y >= this.volumeCore._IJK.dimensions.y) {
+        ijkMin.y = this.volumeCore._IJK.dimensions.y - 1;
+    }
+    if (ijkMin.z < 0) {
+        ijkMin.z = 0;
+    }
+    if (ijkMin.z >= this.volumeCore._IJK.dimensions.z) {
+        ijkMin.z = this.volumeCore._IJK.dimensions.z - 1;
+    }
+    if (ijkMax.x < 0) {
+        ijkMax.x = 0;
+    }
+    if (ijkMax.x >= this.volumeCore._IJK.dimensions.x) {
+        ijkMax.x = this.volumeCore._IJK.dimensions.x - 1;
+    }
+    if (ijkMax.y < 0) {
+        ijkMax.y = 0;
+    }
+    if (ijkMax.y >= this.volumeCore._IJK.dimensions.y) {
+        ijkMax.y = this.volumeCore._IJK.dimensions.y - 1;
+    }
+    if (ijkMax.z < 0) {
+        ijkMax.z = 0;
+    }
+    if (ijkMax.z >= this.volumeCore._IJK.dimensions.z) {
+        ijkMax.z = this.volumeCore._IJK.dimensions.z - 1;
+    }
+
+    // window.console.log(bbox);
+    var roiStats = {
+        points: [],
+        max: -Number.MAX_VALUE,
+        min: Number.MAX_VALUE
+    };
+
+    var roiBox = new THREE.Box3(c0, c7);
+
+
+    for (var i = ijkMin.x; i <= ijkMax.x; i++) {
+        for (var j = ijkMin.y; j <= ijkMax.y; j++) {
+            for (var k = ijkMin.z; k <= ijkMax.z; k++) {
+                // get voxel bbox
+                // draw it as a test...
+                // 0.5 offset?
+                var voxelBox = new THREE.Box3(
+                    new THREE.Vector3(i, j, k),
+                    new THREE.Vector3(i + 1, j + 1, k + 1)
+                );
+                voxelBox.applyMatrix4(this.volumeCore._Transforms.ijk2ras);
+
+                if (voxelBox.intersect(roiBox)) {
+                    var point = {
+                        'ijk': [i, j, k],
+                        'value': this.volumeCore.getValue(i, j, k, 0, false)
+                    };
+                    roiStats.points.push(point);
+                    // compute as much as possible here to avoid having to loop through points again alter...
+                    // sum += value;
+                    // nbVoxels += 1;
+
+                    roiStats.min = (point.value < roiStats.min) ? point.value : roiStats.min;
+                    roiStats.max = (point.value > roiStats.max) ? point.value : roiStats.max;
+                }
+            }
+        }
+    }
+
+    this.updateCore(roiStats);
+
+};
+
 // should just pass the points and do stats here...
 // but might be worst performance-wise
-VJS.Widgets.ProbeROI.prototype.update = function(points) {
+VJS.Widgets.ProbeROI.prototype.updateCore = function(points) {
 
     // ugly
     if (JSON.stringify(points) === '{}') {
