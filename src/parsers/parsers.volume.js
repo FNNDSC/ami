467e282ab441e04456c2e9cfb73051f26d72c32c
nicolasrannou <npm.rannou@gmail.com>
2016-06-02 10:14:57 +0200

    swap 16 and 32 bits convenience for Big Endians

--- src/parsers/parsers.volume.js
+++ src/parsers/parsers.volume.js
@@ -6,4 +6,20 @@ export default class ParsersVolume {
   _decompressUncompressed(){
     
   }
+
+  //http://stackoverflow.com/questions/5320439/how-do-i-swap-endian-ness-byte-order-of-a-variable-in-javascript
+  _swap16(val) {
+
+    return ((val & 0xFF) << 8)
+      | ((val >> 8) & 0xFF);
+
+  }
+
+  _swap32(val) {
+    return ((val & 0xFF) << 24)
+           | ((val & 0xFF00) << 8)
+           | ((val >> 8) & 0xFF00)
+           | ((val >> 24) & 0xFF);
+  }
+
 }
