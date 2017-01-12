e4552b6967b815f8273c335011ac0b6bfbe8843f
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2017-01-12 22:42:35 +0000

    WIP updates.

--- examples/xtk_lesson10/xtk_lesson10.js
+++ examples/xtk_lesson10/xtk_lesson10.js
@@ -7,36 +7,39 @@ import XVolume from '../../src/helpers/x/helpers.x.volume';
 
 window.onload = function() {
 
-  // init the renderer
-  const renderer = new XRenderer3D();
-  renderer.animate();
+    // init the renderer
+    const renderer = new XRenderer3D();
+    renderer.animate();
 
-  // set the mesh, i.e. the 3D object
-  const mesh = new XMesh();
-  mesh._renderer = renderer;
-  mesh._file = 'https://cdn.rawgit.com/FNNDSC/data/master/vtk/marc_avf/avf.vtk';
-  mesh._materialColor = 0xFFEB3B;
-  mesh._intoRenderer_load();
+    // set the mesh, i.e. the 3D object
+    const mesh      = new XMesh();
+    mesh.file       = 'https://cdn.rawgit.com/FNNDSC/data/master/vtk/marc_avf/avf.vtk';
+    mesh.load().then( mesh => {
+        renderer.add(mesh);
+    }).catch( error => {
+        window.console.log('ERROR: something went wrong with the mesh load.');
+        window.console.log(error);
+    });
 
-  // set the 3D volume
-  const t2 = [
-    'avf_float_32.nii.gz'
-  ];
-  const files = t2.map(function(v) {
-    return 'https://cdn.rawgit.com/FNNDSC/data/master/nifti/marc_avf/' + v;
-  });
-  const volume = new XVolume();
-  volume.file = files;
-  volume.progressbar_container = renderer._container;
+    mesh._materialColor = 0xFFEB3B;
+    // mesh._intoRenderer_load();
 
-  volume.load().then( function() {
+    // set the 3D volume
+    const t2 = [
+        'avf_float_32.nii.gz'
+    ];
+    const files = t2.map(function(v) {
+        return 'https://cdn.rawgit.com/FNNDSC/data/master/nifti/marc_avf/' + v;
+    });
+    const volume    = new XVolume();
+    volume.file     = files;
+    volume.progressbar_container = renderer._container;
 
-    renderer.add(volume);
-    renderer.center(volume.centerLPS);
-
-  }).catch( function(error) {
-
-    window.console.log('oops... something went wrong...');
-    window.console.log(error);
-  });
+    volume.load().then( function() {
+        renderer.add(volume);
+        renderer.center(volume.centerLPS);
+    }).catch( function(error) {
+        window.console.log('ERROR: something went wrong with the vol load.');
+        window.console.log(error);
+    });
 };
