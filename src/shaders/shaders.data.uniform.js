780870ac8dbda963df5be5fc55cbc13e80b78bc6
tymiao1220 <tymiao@gwmail.gwu.edu>
2018-02-27 14:46:59 -0500

    Add thresholding

--- src/shaders/shaders.data.uniform.js
+++ src/shaders/shaders.data.uniform.js
@@ -37,6 +37,12 @@ export default class ShadersUniform {
         typeGLSL: 'float',
         length: 2,
       },
+      'uUpperLowerThreshold': {
+        type: 'fv1',
+        value: [0.0, 0.0],
+        typeGLSL: 'float',
+        length: 2,
+      },
       'uRescaleSlopeIntercept': {
         type: 'fv1',
         value: [0.0, 0.0],
