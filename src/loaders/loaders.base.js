d885d494dfa6f513e09cb9297d345d7ddd899e74
Evgeny Sibirtsev <evgeny.sibirtsev@unvsoft.com>
2018-03-26 10:34:53 +0300

    ability to cancel load from progress bar object

--- src/loaders/loaders.base.js
+++ src/loaders/loaders.base.js
@@ -259,6 +259,7 @@ export default class LoadersBase extends EventEmitter {
 
     if (this._progressBar) {
       this._progressBar.totalFiles = url.length;
+      this._progressBar.cancellationToken = token;
     }
 
     // emit 'load-start' event
