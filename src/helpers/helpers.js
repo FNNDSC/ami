63e81c00726615279a427ad3cbdde83b4ffc9cfc
nicolasrannou <npm.rannou@gmail.com>
2018-06-22 11:38:48 +0200

    wip: factory pattern helpers

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,17 +1,20 @@
-import Border from './helpers.border';
-import BoundingBox from './helpers.boundingbox';
+import Border, {helpersBorder} from './helpers.border';
+import BoundingBox, {helpersBoundingBox} from './helpers.boundingbox';
 import Contour from './helpers.contour';
 import Localizer from './helpers.localizer';
 import Lut from './helpers.lut';
 import SegmentationLut from './helpers.segmentationlut';
 import ProgressBar from './helpers.progressbar';
 import ProgressBarEventBased from './helpers.progressbar.eventbased';
-import Slice from './helpers.slice';
-import Stack from './helpers.stack';
+import Slice, {helpersGeometry} from './helpers.slice';
+import Stack, {helpersStack} from './helpers.stack';
 import VolumeRendering from './helpers.volumerendering';
+
 export {
     Border as BorderHelper,
+    helpersBorder as borderHelperFactory,
     BoundingBox as BoundingBoxHelper,
+    helpersBoundingBox as boundingBoxHelperFactory,
     Contour as ContourHelper,
     Localizer as LocalizerHelper,
     Lut as LutHelper,
@@ -19,6 +22,8 @@ export {
     ProgressBar as ProgressBarHelper,
     ProgressBarEventBased as ProgressBarEventBasedHelper,
     Slice as SliceHelper,
+    helpersGeometry as geometryHelperFactory,
     Stack as StackHelper,
+    helpersStack as stackHelperFactory,
     VolumeRendering as VolumeRenderingHelper,
 };
