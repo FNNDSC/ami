066f064cf6639941cb3c7b6a7d5a09eab6dcb5d2
Evgeny <jsibir@gmail.com>
2018-07-16 12:12:11 +0300

    URL parse fix for API (#317)

--- src/core/core.utils.js
+++ src/core/core.utils.js
@@ -116,17 +116,14 @@ export default class CoreUtils {
    * @return {Object}
    */
   static parseUrl(url) {
-    const data = {};
-    data.filename = '';
-    data.extension = '';
-    data.pathname = '';
-    data.query = '';
-
-    let parsedUrl = new URL(url);
-
-    data.pathname = parsedUrl.pathname;
-    data.query = parsedUrl.search;
-    data.filename = parsedUrl.searchParams.get('filename');
+    const absUrlRe = /^(?:[a-z]+:)?\/\//i,
+      parsedUrl = new URL(url, absUrlRe.test(url) ? '' : location.origin),
+      data = {
+        filename: parsedUrl.searchParams.get('filename'),
+        extension: '',
+        pathname: parsedUrl.pathname,
+        query: parsedUrl.search,
+      };
 
     // get file name
     if (!data.filename) {
@@ -134,19 +131,14 @@ export default class CoreUtils {
     }
 
     // find extension
-    let splittedName = data.filename.split('.');
-    if (splittedName.length <= 1) {
-      data.extension = 'dicom';
-    } else {
-      data.extension = data.filename.split('.').pop();
-    }
+    const splittedName = data.filename.split('.');
+    data.extension = splittedName.length > 1 ? splittedName.pop() : 'dicom';
 
-    if (!isNaN(data.extension)) {
-      data.extension = 'dicom';
-    }
-
-    if (data.query &&
-      data.query.includes('contentType=application%2Fdicom')) {
+    const skipExt = ['asp', 'aspx', 'go', 'gs', 'hs', 'jsp', 'js', 'php', 'pl', 'py', 'rb', 'htm', 'html'];
+    if (!isNaN(data.extension) ||
+      skipExt.indexOf(data.extension) !== -1 ||
+      (data.query && data.query.includes('contentType=application%2Fdicom'))
+    ) {
       data.extension = 'dicom';
     }
 
