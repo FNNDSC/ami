dfe09a2c79bdce3604a855b2022f94cffc53a637
NicolasRannou <npm.rannou@gmail.com>
2018-11-07 11:43:11 +0100

    Add TS definitions to build directory

---
+++ build/ami.d.ts
@@ -0,0 +1,12 @@
+export * from './cameras/cameras';
+export * from './controls/controls';
+export * from './core/core';
+export * from './geometries/geometries';
+export * from './helpers/helpers';
+export * from './loaders/loaders';
+export * from './models/models';
+export * from './parsers/parsers';
+export * from './presets/presets';
+export * from './shaders/shaders';
+export * from './widgets/widgets';
+//# sourceMappingURL=ami.d.ts.map
