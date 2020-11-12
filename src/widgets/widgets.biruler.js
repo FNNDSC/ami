7e3e6f22c0a3581b4fc943a89d58423d439b753a
Evgenii Sibirtcev <jsibir@gmail.com>
2020-11-12 14:05:58 +0300

    BiRuler: param cobbAngle and new label are added, behavior is updated; corrections

--- src/widgets/widgets.biruler.js
+++ src/widgets/widgets.biruler.js
@@ -18,11 +18,16 @@ const widgetsBiruler = (three = window.THREE) => {
 
       // incoming parameters (optional: lps2IJK, pixelSpacing, ultrasoundRegions, worldPosition)
       this._calibrationFactor = params.calibrationFactor || null;
+      this._cobbAngle = params.cobbAngle || false;
 
       // outgoing values
+      this._angle = null;
       this._distance = null;
       this._distance2 = null;
       this._units = !this._calibrationFactor && !params.pixelSpacing ? 'units' : 'mm';
+
+      this._domHovered = false;
+      this._moving = false;
 
       // mesh stuff
       this._material = null;
@@ -35,6 +40,7 @@ const widgetsBiruler = (three = window.THREE) => {
       this._line2 = null;
       this._label2 = null;
       this._dashline = null;
+      this._labelAngle = null;
 
       // add handles
       this._handles = [];
@@ -48,59 +54,118 @@ const widgetsBiruler = (three = window.THREE) => {
       }
       this._handles[1].active = true;
       this._handles[1].tracking = true;
+      this._handles[2].active = true;
+      this._handles[2].tracking = true;
       this._handles[3].active = true;
       this._handles[3].tracking = true;
 
+      this._moveHandle = new WidgetsHandle(targetMesh, controls, params);
+      this.add(this._moveHandle);
+      this._handles.push(this._moveHandle);
+      this._moveHandle.hide();
+
       this.create();
 
+      this.onHover = this.onHover.bind(this);
       this.onMove = this.onMove.bind(this);
       this.addEventListeners();
     }
 
     addEventListeners() {
+      this._line.addEventListener('mouseenter', this.onHover);
+      this._line.addEventListener('mouseleave', this.onHover);
+      this._line2.addEventListener('mouseenter', this.onHover);
+      this._line2.addEventListener('mouseleave', this.onHover);
+
       this._container.addEventListener('wheel', this.onMove);
     }
 
     removeEventListeners() {
+      this._line.removeEventListener('mouseenter', this.onHover);
+      this._line.removeEventListener('mouseleave', this.onHover);
+      this._line2.removeEventListener('mouseenter', this.onHover);
+      this._line2.removeEventListener('mouseleave', this.onHover);
+
       this._container.removeEventListener('wheel', this.onMove);
     }
 
+    onHover(evt) {
+      if (evt) {
+        this.hoverDom(evt);
+      }
+
+      this.hoverMesh();
+
+      this._hovered =
+          this._handles[0].hovered ||
+          this._handles[1].hovered ||
+          this._handles[2].hovered ||
+          this._handles[3].hovered ||
+          this._domHovered;
+      this._container.style.cursor = this._hovered ? 'pointer' : 'default';
+    }
+
+    hoverMesh() {
+      // check raycast intersection, do we want to hover on mesh or just css?
+    }
+
+    hoverDom(evt) {
+      this._domHovered = evt.type === 'mouseenter';
+    }
+
     onStart(evt) {
-      this._handles.forEach(elem => elem.onStart(evt));
+      this._moveHandle.onMove(evt, true);
+
+      this._handles.slice(0, -1).forEach(elem => elem.onStart(evt));
 
       this._active =
         this._handles[0].active ||
         this._handles[1].active ||
         this._handles[2].active ||
-        this._handles[3].active;
+        this._handles[3].active ||
+        this._domHovered;
+
+      if (this._domHovered && !this._handles[3].tracking) {
+        this._moving = true;
+        this._controls.enabled = false;
+      }
 
       this.update();
     }
 
     onMove(evt) {
       if (this._active) {
+        const prevPosition = this._moveHandle.worldPosition.clone();
+
         this._dragged = true;
+        this._moveHandle.onMove(evt, true);
+
+        if (this._moving) {
+          this._handles.slice(0, -1).forEach(handle => {
+            handle.worldPosition.add(this._moveHandle.worldPosition.clone().sub(prevPosition));
+          });
+        }
       } else {
-        this._hovered =
-          this._handles[0].hovered ||
-          this._handles[1].hovered ||
-          this._handles[2].hovered ||
-          this._handles[3].hovered;
-        this._container.style.cursor = this._hovered ? 'pointer' : 'default';
+        this.onHover(null);
       }
 
-      this._handles.forEach(elem => elem.onMove(evt));
+      this._handles.slice(0, -1).forEach(elem => elem.onMove(evt));
 
       this.update();
     }
 
     onEnd() {
-      this._handles[0].onEnd();
-      this._handles[2].onEnd();
+      this._handles[0].onEnd(); // First Handle
 
       if (
-        this._handles[1].tracking &&
-        this._handles[0].screenPosition.distanceTo(this._handles[1].screenPosition) < 10
+        (this._handles[1].tracking &&
+            this._handles[0].screenPosition.distanceTo(this._handles[1].screenPosition) < 10) ||
+        (!this._handles[1].tracking &&
+            this._handles[2].tracking &&
+            this._handles[1].screenPosition.distanceTo(this._handles[2].screenPosition) < 10) ||
+        (!this._handles[2].tracking &&
+            this._handles[3].tracking &&
+            this._handles[2].screenPosition.distanceTo(this._handles[3].screenPosition) < 10)
       ) {
         return;
       }
@@ -108,11 +173,10 @@ const widgetsBiruler = (three = window.THREE) => {
       if (!this._dragged && this._active && !this._handles[3].tracking) {
         this._selected = !this._selected; // change state if there was no dragging
         this._handles[0].selected = this._selected;
-        this._handles[2].selected = this._selected;
       }
 
       // Fourth Handle
-      if (this._handles[1].active) {
+      if (this._handles[2].active) {
         this._handles[3].onEnd();
       } else if (this._dragged || !this._handles[3].tracking) {
         this._handles[3].tracking = false;
@@ -121,6 +185,17 @@ const widgetsBiruler = (three = window.THREE) => {
         this._handles[3].tracking = false;
       }
       this._handles[3].selected = this._selected;
+
+      // Third Handle
+      if (this._handles[1].active) {
+        this._handles[2].onEnd();
+      } else if (this._dragged || !this._handles[2].tracking) {
+        this._handles[2].tracking = false;
+        this._handles[2].onEnd();
+      } else {
+        this._handles[2].tracking = false;
+      }
+      this._handles[2].selected = this._selected;
 
       // Second Handle
       if (this._dragged || !this._handles[1].tracking) {
@@ -136,7 +211,9 @@ const widgetsBiruler = (three = window.THREE) => {
         this._handles[1].active ||
         this._handles[2].active ||
         this._handles[3].active;
-      this._dragged = false;
+      this._dragged = false; // TODO! ??? or this._dragged = this._handles[2].tracking;
+      this._moving = false;
+
       this.update();
     }
 
@@ -171,49 +248,60 @@ const widgetsBiruler = (three = window.THREE) => {
       this._line.className = 'widgets-line';
       this._container.appendChild(this._line);
 
-      this._label = document.createElement('div');
-      this._label.className = 'widgets-label';
-      this._container.appendChild(this._label);
-
       this._line2 = document.createElement('div');
       this._line2.className = 'widgets-line';
       this._container.appendChild(this._line2);
+
+      this._dashline = document.createElement('div');
+      this._dashline.className = 'widgets-dashline';
+      this._container.appendChild(this._dashline);
+
+      this._label = document.createElement('div');
+      this._label.className = 'widgets-label';
+      this._container.appendChild(this._label);
 
       this._label2 = document.createElement('div');
       this._label2.className = 'widgets-label';
       this._container.appendChild(this._label2);
 
-      this._dashline = document.createElement('div');
-      this._dashline.className = 'widgets-dashline';
-      this._container.appendChild(this._dashline);
+      this._labelAngle = document.createElement('div');
+      this._labelAngle.className = 'widgets-label';
+      this._container.appendChild(this._labelAngle);
 
       this.updateDOMColor();
     }
 
     hideDOM() {
       this._line.style.display = 'none';
-      this._label.style.display = 'none';
       this._line2.style.display = 'none';
-      this._label2.style.display = 'none';
       this._dashline.style.display = 'none';
+
+      this._label.style.display = 'none';
+      this._label2.style.display = 'none';
+      this._labelAngle.style.display = 'none';
 
       this._handles.forEach(elem => elem.hideDOM());
     }
 
     showDOM() {
       this._line.style.display = '';
-      this._label.style.display = '';
       this._line2.style.display = '';
-      this._label2.style.display = '';
       this._dashline.style.display = '';
 
-      this._handles.forEach(elem => elem.showDOM());
+      if (this._cobbAngle) {
+        this._labelAngle.style.display = '';
+      } else {
+        this._label.style.display = '';
+        this._label2.style.display = '';
+      }
+
+      this._handles.slice(0, -1).forEach(elem => elem.showDOM());
     }
 
     update() {
       this.updateColor();
 
-      this._handles.forEach(elem => elem.update());
+      this._handles.slice(0, -1).forEach(elem => elem.update());
 
       this.updateMeshColor();
       this.updateMeshPosition();
@@ -261,17 +349,9 @@ const widgetsBiruler = (three = window.THREE) => {
       this._line2.style.width = line2Data.length + 'px';
 
       // update dash line
-      const line1Center = this._handles[0].worldPosition
-        .clone()
-        .add(this._handles[1].worldPosition)
-        .multiplyScalar(0.5);
-      const line2Center = this._handles[2].worldPosition
-        .clone()
-        .add(this._handles[3].worldPosition)
-        .multiplyScalar(0.5);
       const dashLineData = this.getLineData(
-        this.worldToScreen(line1Center),
-        this.worldToScreen(line2Center)
+        this.worldToScreen(lineData.center),
+        this.worldToScreen(line2Data.center)
       );
 
       this._dashline.style.transform = `translate3D(${dashLineData.transformX}px, ${
@@ -280,7 +360,7 @@ const widgetsBiruler = (three = window.THREE) => {
                 rotate(${dashLineData.transformAngle}rad)`;
       this._dashline.style.width = dashLineData.length + 'px';
 
-      // update labels
+      // update distance labels
       const distanceData = this.getDistanceData(
         this._handles[0].worldPosition,
         this._handles[1].worldPosition,
@@ -358,6 +438,28 @@ const widgetsBiruler = (three = window.THREE) => {
       const transform2 = this.adjustLabelTransform(this._label2, paddingPoint2);
 
       this._label2.style.transform = `translate3D(${transform2.x}px, ${transform2.y}px, 0)`;
+
+      // update angle label
+      this._angle = (lineData.line.angleTo(line2Data.line) * 180) / Math.PI || 0.0;
+
+      this._labelAngle.innerHTML = `${this._angle.toFixed(2)}&deg;`;
+
+      /* TODO! do we want to put label on line on near it ?
+      let angleA = Math.abs(dashLineData.transformAngle);
+      if (angleA > Math.PI / 2) {
+        angleA = Math.PI - angleA;
+      }
+
+      const labelAPadding = Math.tan(angleA) < this._labelAngle.offsetHeight / this._labelAngle.offsetWidth
+        ? this._labelAngle.offsetWidth / 2 / Math.cos(angleA) + 15 // 5px for each handle + padding
+        : this._labelAngle.offsetHeight / 2 / Math.cos(Math.PI / 2 - angleA) + 15;
+      const paddingAVector = dashLineData.line.normalize().multiplyScalar(labelAPadding);
+      const paddingAPoint = dashLineData.length > labelAPadding * 2
+        ? this._handles[1].screenPosition.clone().sub(paddingAVector)
+        : this._handles[1].screenPosition.clone().add(paddingAVector);*/
+      const transformA = this.adjustLabelTransform(this._labelAngle, dashLineData.center);
+
+      this._labelAngle.style.transform = `translate3D(${transformA.x}px, ${transformA.y}px, 0)`;
     }
 
     updateDOMColor() {
@@ -368,6 +470,7 @@ const widgetsBiruler = (three = window.THREE) => {
       this._label2.style.borderColor = this._color;
 
       this._dashline.style.borderTop = '1.5px dashed ' + this._color;
+      this._labelAngle.style.borderColor = this._color;
     }
 
     free() {
@@ -384,6 +487,7 @@ const widgetsBiruler = (three = window.THREE) => {
       this._container.removeChild(this._line2);
       this._container.removeChild(this._label2);
       this._container.removeChild(this._dashline);
+      this._container.removeChild(this._labelAngle);
 
       // mesh, geometry, material
       this.remove(this._mesh);
@@ -427,7 +531,7 @@ const widgetsBiruler = (three = window.THREE) => {
     }
 
     set worldPosition(worldPosition) {
-      this._handles.forEach(elem => elem.worldPosition.copy(worldPosition));
+      this._handles.slice(0, -1).forEach(elem => elem.worldPosition.copy(worldPosition));
       this._worldPosition.copy(worldPosition);
       this.update();
     }
@@ -442,7 +546,11 @@ const widgetsBiruler = (three = window.THREE) => {
       this.update();
     }
 
-    get shotestDistance() {
+    get angle() {
+      return this._angle;
+    }
+
+    get shortestDistance() {
       return this._distance < this._distance2 ? this._distance : this._distance2;
     }
 
